aed9002ae273ee2fa5f3816663cda833aab56bc1
Daniela Salas <danielapiconsalas@gmail.com>
2023-06-28 11:45:51 -0500

    fixed some issues with the points functionality

--- js/script.js
+++ js/script.js
@@ -1,30 +1,44 @@
 let points = 0;
+let intervalId;
+
 function showRandomMole() {
     const boxes = document.querySelectorAll('.box');
     const reset = document.querySelector('#reset');
 
     const randomBox = Math.floor(Math.random() * boxes.length);
-
     const selectedBox = boxes[randomBox];
 
-    selectedBox.classList.add('mole');
-    selectedBox.style.animation = 'fadeIn 1s';
+    if (!selectedBox.classList.contains('mole')) {
+        selectedBox.classList.add('mole');
+        selectedBox.style.animation = 'fadeIn 1s';
+        selectedBox.addEventListener('click', moleClicked);
+    }
 
-    selectedBox.addEventListener('click', () => {
-        points++; // Increment the score when the user clicks on a mole
-        document.getElementById('points-display').textContent = `Points: ${points}`;
-    });
+    function moleClicked() {
+        if (selectedBox.classList.contains('mole')) {
+            points++;
+            console.log("points: " + points)
+            document.getElementById('points-display').textContent = `Points: ${points}`;
+            clearInterval(intervalId);
+            intervalId = setInterval(showRandomMole, 2000);
+            selectedBox.removeEventListener('click', moleClicked);
+        }
+    }
 
     reset.addEventListener('click', () => {
         points = 0;
+        console.log("reset points: " + points)
         document.getElementById('points-display').textContent = `Points: ${points}`;
+        clearInterval(intervalId);
+        intervalId = setInterval(showRandomMole, 2000);
     });
 
 
     setTimeout(() => {
         selectedBox.classList.remove('mole');
         selectedBox.style.animation = '';
-    }, 1000);
+        selectedBox.removeEventListener('click', moleClicked);
+    }, 2000);
 }
 
-setInterval(showRandomMole, 1000);
+setInterval(showRandomMole, 2000);
